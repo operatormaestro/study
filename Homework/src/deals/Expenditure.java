27981d681985b756c49838b1dfead8b6f59d270f
Mikhail Semavin <operator.mixa@mail.ru>
2023-02-13 23:32:16 +0300

    Система налогообложения, ДЗ от нетологии. Доработанная версия, исправлены конструкторы классов Sale и Expenditure, удален лишний пустой уонструктор Deal.

--- Homework/src/deals/Expenditure.java
+++ Homework/src/deals/Expenditure.java
@@ -2,6 +2,6 @@ package deals;
 
 public class Expenditure extends Deal {
     public Expenditure(String nameOfProduct, int price) {
-        super("Покупка " + nameOfProduct + " на " + price + " руб.",price,0);
+        super("Покупка " + nameOfProduct + " на " + price + " руб.", price, 0);
     }
 }
